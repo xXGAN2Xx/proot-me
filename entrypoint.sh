9c4467045ba5917257ea6d11e48c6ca7ce5f859f
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-06-22 13:34:47 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 if [ ! -e "$HOME/.installed" ]; then
     ${HOME}/usr/local/bin/proot \
     --rootfs="/" \
-    -0 -w "/root" \
+    -0 -l -w "/root" \
     -b /dev -b /sys -b /proc -b /etc/resolv.conf \
     --kill-on-exit \
     /bin/bash "${HOME}/install.sh" || exit 1
